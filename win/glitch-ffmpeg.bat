b109878a4c1cd7a6b20f24e625f79f1a3757f700
Nick Fox-Gieg <nick@fox-gieg.com>
2020-03-19 13:06:06 -0400

    Update glitch-ffmpeg.bat

--- win/glitch-ffmpeg.bat
+++ win/glitch-ffmpeg.bat
@@ -1,5 +1,8 @@
 @echo off
 
-ffmpeg -i cat_rainbow_original.mp4 -filter:v "setpts=62.5*PTS,minterpolate='fps=25:mb_size=16:search_param=400:vsbmc=0:scd=none:'" test.mp4
+set INPUT_NAME=%~1
+set OUTPUT_NAME=%~dpn1
+
+ffmpeg -i %INPUT_NAME% -filter:v "setpts=62.5*PTS,minterpolate='fps=25:mb_size=16:search_param=400:vsbmc=0:scd=none:'" %OUTPUT_NAME%_glitch.mp4
 
 @pause
